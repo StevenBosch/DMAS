6d29f6df94b5e0496532c188b53505b021b3f515
Maikel School <m.l.c.withagen@student.rug.nl>
2015-10-01 11:18:14 +0200

    code cleanup

--- CivilViolence/src/civilviolence/Dmas.java
+++ CivilViolence/src/civilviolence/Dmas.java
@@ -1,11 +1,8 @@
 package civilviolence;
 
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.Color;
 import java.util.HashMap;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 
 /**
@@ -187,86 +184,8 @@ public class Dmas implements ActionListener {
         JFrame frame = new JFrame();
 
         // Import/Edit the layout to show the griddy
-        final GUIFrame gFrame = new GUIFrame();
-        gFrame.GridPanel.setLayout(
-                new GridLayout(
-                        param.get("LENGTH"),
-                        param.get("WIDTH")
-                ));
-        gFrame.ControlFrame.setLayout(new GridLayout(1,3));
-
-        gFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-
-        // Get the screen size as a java dimension
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-
-        gFrame.jSplitPane1.setDividerLocation(screenSize.height);
-        gFrame.jSplitPane1.setEnabled(false);
-
-        // Add the grid buttons
-        for (int row = 0; row < param.get("LENGTH"); ++row) {
-            for (int col = 0; col < param.get("WIDTH"); ++col) {
-
-                // Variables to be used in the action listener
-                // (have to be final for some reason)
-                final int finalRow = row;
-                final int finalCol = col;
-                final int nrNeutral = grid[row][col].getNrNeutral();
-                final int nrHostiles = grid[row][col].getNrHostiles();
-                final int nrCops = grid[row][col].getNrGood();
-
-                // Create the button
-                JButton btn = new javax.swing.JButton();
-
-                // Set a random background
-                if (nrHostiles == 0) {
-                    btn.setBackground(new Color(0, 0, 255));
-                } else if (nrCops == 0 & nrHostiles != 0) {
-                    btn.setBackground(new Color(255, 0, 0));
-                } else {
-                    // Range from -1 to 1
-                    float temp = ((((float) (nrCops - nrHostiles) / (nrCops + nrHostiles)) + 1) / 2) * 255;
-                    btn.setBackground(new Color(
-                            (int) (255 - temp),
-                            0,
-                            (int) temp
-                    ));
-                }
-                // Set the text as number of neutrals
-                btn.setFont(new Font("Arial", Font.PLAIN, 12));
-                btn.setMargin(new Insets(0, 0, 0, 0));
-                btn.setForeground(Color.WHITE);
-                btn.setText("" + grid[row][col].getNrNeutral());
-
-                // Adjust the information box on buttonclick
-                // btn.addActionListener((ActionEvent e) -> {
-                btn.addActionListener(new ActionListener() {
-                    public void actionPerformed(ActionEvent event) {
-                        gFrame.infoField.setText(
-                                "\n\n\t Number of neutrals on this site:\t" + nrNeutral + '\n'
-                                + "\t Number of hostiles on this site:\t" + nrHostiles + '\n'
-                                + "\t Number of cops on this site:\t\t" + nrCops
-                                + "\n\n"
-                                + "\t Total of neutral on this site:\t" + param.get("TOTALNRNEUTRAL") + '\n'
-                                + "\t Total of hostiles on this site:\t" + param.get("TOTALNRHOSTILES")
-                        );
-                    }
-                });
-
-                // Add the button to the group and the panel
-                gFrame.buttonGroup1.add(btn);
-                gFrame.GridPanel.add(btn);
-            }
-        }
-
-        // Add the control buttons
-        JButton btn = new javax.swing.JButton("Knopje 1");
-        gFrame.ControlFrame.add(btn);
-        JButton btn2 = new javax.swing.JButton("Knopje 2");
-        gFrame.ControlFrame.add(btn2);
-        JButton btn3 = new javax.swing.JButton("Knopje 3");
-        gFrame.ControlFrame.add(btn3);
-
+        final GUIFrame gFrame = new GUIFrame(grid, param);
+        
         // Display the gui frame
         gFrame.setVisible(true);
 
--- CivilViolence/src/civilviolence/GUIFrame.java
+++ CivilViolence/src/civilviolence/GUIFrame.java
@@ -5,6 +5,18 @@
  */
 package civilviolence;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 /**
  *
  * @author maleco
@@ -13,9 +25,97 @@ public class GUIFrame extends javax.swing.JFrame {
 
     /**
      * Creates new form NewJFrame
+     *
+     * @param grid
+     * @param param
      */
-    public GUIFrame() {
+    public GUIFrame(Cell[][] grid, final HashMap<String, Integer> param) {
         initComponents();
+
+        // Set the layouts
+        GridPanel.setLayout(
+                new GridLayout(
+                        param.get("LENGTH"),
+                        param.get("WIDTH")
+                ));
+        ControlFrame.setLayout(new GridLayout(1, 3));
+
+        // Maximize the screen
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+        // Set the panel split
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        jSplitPane1.setDividerLocation(screenSize.height);
+        jSplitPane1.setEnabled(false);
+
+        // Add the grid buttons
+        setGridButtons(grid, param);
+        
+        // Add the control buttons
+        JButton btn = new javax.swing.JButton("Knopje 1");
+        ControlFrame.add(btn);
+        JButton btn2 = new javax.swing.JButton("Knopje 2");
+        ControlFrame.add(btn2);
+        JButton btn3 = new javax.swing.JButton("Knopje 3");
+        ControlFrame.add(btn3);
+    }
+
+    private void setGridButtons(Cell[][] grid, final HashMap<String, Integer> param) {
+        // Add the grid buttons
+        for (int row = 0; row < param.get("LENGTH"); ++row) {
+            for (int col = 0; col < param.get("WIDTH"); ++col) {
+
+                // Variables to be used in the action listener
+                // (have to be final for some reason)
+                final int finalRow = row;
+                final int finalCol = col;
+                final int nrNeutral = grid[row][col].getNrNeutral();
+                final int nrHostiles = grid[row][col].getNrHostiles();
+                final int nrCops = grid[row][col].getNrGood();
+
+                // Create the button
+                JButton btn = new javax.swing.JButton();
+
+                // Set a random background
+                if (nrHostiles == 0) {
+                    btn.setBackground(new Color(0, 0, 255));
+                } else if (nrCops == 0 & nrHostiles != 0) {
+                    btn.setBackground(new Color(255, 0, 0));
+                } else {
+                    // Range from -1 to 1
+                    float temp = ((((float) (nrCops - nrHostiles) / (nrCops + nrHostiles)) + 1) / 2) * 255;
+                    btn.setBackground(new Color(
+                            (int) (255 - temp),
+                            0,
+                            (int) temp
+                    ));
+                }
+                // Set the text as number of neutrals
+                btn.setFont(new Font("Arial", Font.PLAIN, 12));
+                btn.setMargin(new Insets(0, 0, 0, 0));
+                btn.setForeground(Color.WHITE);
+                btn.setText("" + grid[row][col].getNrNeutral());
+
+                // Adjust the information box on buttonclick
+                // btn.addActionListener((ActionEvent e) -> {
+                btn.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent event) {
+                        infoField.setText(
+                                "\n\n\t Number of neutrals on this site:\t" + nrNeutral + '\n'
+                                + "\t Number of hostiles on this site:\t" + nrHostiles + '\n'
+                                + "\t Number of cops on this site:\t\t" + nrCops
+                                + "\n\n"
+                                + "\t Total of neutral on this site:\t" + param.get("TOTALNRNEUTRAL") + '\n'
+                                + "\t Total of hostiles on this site:\t" + param.get("TOTALNRHOSTILES")
+                        );
+                    }
+                });
+
+                // Add the button to the group and the panel
+                buttonGroup1.add(btn);
+                GridPanel.add(btn);
+            }
+        }
     }
 
     /**
@@ -116,11 +216,6 @@ public class GUIFrame extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new GUIFrame().setVisible(true);
-            }
-        });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
