ff7b526fe3d3c4cf7f9b6171ebbd46545491a328
Maikel School <m.l.c.withagen@student.rug.nl>
2015-10-13 10:42:58 +0200

    woei

--- CivilViolence/src/civilviolence/GUIFrame.java
+++ CivilViolence/src/civilviolence/GUIFrame.java
@@ -9,9 +9,7 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.DecimalFormat;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
